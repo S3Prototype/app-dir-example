58f2244ee452ee65ae1bfd1fba9cce9e222b0d1d
S3Prototype <liuqahs15@gmail.com>
2023-07-20 07:53:31 -0500

    Make cache-control example route serverless

--- app/api/cache-control-example/route.tsx
+++ app/api/cache-control-example/route.tsx
@@ -1,21 +1,3 @@
-import { NextResponse } from "next/server";
-
-export const runtime = "edge";
-
-// export async function GET(request: Request) {
-//   const cacheControlHeaders = new Headers(request.headers);
-//   cacheControlHeaders.set("Vercel-CDN-Cache-Control", "public, s-maxage=3600");
-//   cacheControlHeaders.set("CDN-Cache-Control", "public, s-maxage=60");
-//   cacheControlHeaders.set("Cache-Control", "public, max-age=1");
-//   return NextResponse.json(
-//     { name: "John Doe" },
-//     {
-//       status: 200,
-//       headers: cacheControlHeaders,
-//     }
-//   );
-// }
-
 export async function GET(request: Request) {
   return new Response("Cache Control example", {
     status: 200,
